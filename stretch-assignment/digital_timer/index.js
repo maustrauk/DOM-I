aa0e7d0b3d4f2f22667a17999e73de971cd81a2d
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-28 22:49:45 -0400

    bug fix 1

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,9 +1,13 @@
 let startButton = document.createElement('button');
+let resetButton = document.createElement('button');
 startButton.textContent = "START";
 startButton.style.fontSize = "2em";
+resetButton.textContent = "RESET";
+resetButton.style.fontSize = "2em";
 document.querySelector('body').prepend(startButton);
+document.querySelector('body').append(resetButton);
 
-let digits = document.querySelectorAll(".digit");
+let digits = document.querySelector(".digits");
 let secondTens = document.querySelector('#secondTens');
 let secondOnes = document.querySelector('#secondOnes');
 let msHundreds = document.querySelector('#msHundreds');
@@ -18,35 +22,30 @@ function timer (element) {
 }
 
 
-function myTimer () {
-    
-    startButton.disabled = true;
 
-    function final () {
-        clearInterval(msTenstimer);
-        clearInterval(msHundredstimer);
-        clearInterval(secondOnestimer);
-        clearInterval(secondTenstimer);
-        msHundreds.textContent = 0;
-        msTens.textContent = 0;
-        digits.forEach(digit => digit.style.color = 'red');
-        startButton.disabled = false;
-    }
-    
-
-    secondTens.textContent = 0;
-    secondOnes.textContent = 0;
-    msHundreds.textContent = 0;
+startButton.addEventListener('click', () => {
     msTens.textContent = 0;
+    msHundreds.textContent = 0;
+    secondOnes.textContent = 0;
+    secondTens.textContent = 0;
+    const timer1 = setInterval(timer, 10, msTens);
+    const timer2 = setInterval(timer, 100, msHundreds);
+    const timer3 = setInterval(timer, 1000, secondOnes);
+    const timer4 = setInterval(timer, 10000, secondTens);
+    setTimeout(timerTimeOut, 10000, timer1, timer2, timer3, timer4);
+});
 
-    let msTenstimer = setInterval(timer, 10, msTens);
-    let msHundredstimer = setInterval(timer, 100, msHundreds);
-    let secondOnestimer = setInterval(timer, 1000, secondOnes);
-    let secondTenstimer = setInterval(timer, 10000, secondTens);
-
-    setInterval(final, 10000);
+function timerTimeOut (timer1, timer2, timer3, timer4) {
+    console.log("timerOut");
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    clearTimeout(timer3);
+    clearTimeout(timer4);
+    msTens.textContent = 0;
+    msHundreds.textContent = 0;
+    secondOnes.textContent = 0;
+    secondTens.textContent = 1;
+    digits.style.color = 'red';
 }
-
-startButton.addEventListener('click', myTimer);
 
 
