a6368f84c41e5314d80768fc7700ad04d2b20a32
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-28 23:14:19 -0400

    SG3-4

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -21,20 +21,6 @@ function timer (element) {
     element.textContent = counter + 1;
 }
 
-
-
-startButton.addEventListener('click', () => {
-    msTens.textContent = 0;
-    msHundreds.textContent = 0;
-    secondOnes.textContent = 0;
-    secondTens.textContent = 0;
-    const timer1 = setInterval(timer, 10, msTens);
-    const timer2 = setInterval(timer, 100, msHundreds);
-    const timer3 = setInterval(timer, 1000, secondOnes);
-    const timer4 = setInterval(timer, 10000, secondTens);
-    setTimeout(timerTimeOut, 10000, timer1, timer2, timer3, timer4);
-});
-
 function timerTimeOut (timer1, timer2, timer3, timer4) {
     console.log("timerOut");
     clearTimeout(timer1);
@@ -46,6 +32,42 @@ function timerTimeOut (timer1, timer2, timer3, timer4) {
     secondOnes.textContent = 0;
     secondTens.textContent = 1;
     digits.style.color = 'red';
+    startButton.style.background = 'red';
 }
+
+let timer1;
+let timer2;
+let timer3;
+let timer4;
+let globalTimer;
+
+startButton.addEventListener('click', () => {
+    startButton.style.background = "green";
+    digits.classList.add('counter-on');
+    msTens.textContent = 0;
+    msHundreds.textContent = 0;
+    secondOnes.textContent = 0;
+    secondTens.textContent = 0;
+    timer1 = setInterval(timer, 10, msTens);
+    timer2 = setInterval(timer, 100, msHundreds);
+    timer3 = setInterval(timer, 1000, secondOnes);
+    timer4 = setInterval(timer, 10000, secondTens);
+    globalTimer = setTimeout(timerTimeOut, 10000, timer1, timer2, timer3, timer4);
+});
+
+resetButton.addEventListener('click', () => {
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    clearTimeout(timer3);
+    clearTimeout(timer4);
+    clearTimeout(globalTimer);
+    startButton.style.background = "yellow";
+    resetButton.style.background = "yellow";
+    digits.classList.remove('counter-on');
+    msTens.textContent = 0;
+    msHundreds.textContent = 0;
+    secondOnes.textContent = 0;
+    secondTens.textContent = 0;
+})
 
 
